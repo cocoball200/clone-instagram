605024e1ddbdcfc345f9162379f89b994b205371
mia <cocoball200@gmail.com>
2021-01-18 11:09:56 +0900

    first commit

---
+++ login.js
@@ -0,0 +1,11 @@
+const $inputContainer = document.querySelector(".input-container");
+const $inputID = document.querySelector(".input-id");
+const $inputPassword = document.querySelector(".input-password");
+const $loginButton = document.querySelector(".login-button");
+
+const handleLoginButton = () => {
+    $inputID.value.length >= 1 && $inputPassword.value.length >= 5 ? $loginButton.style.backgroundColor = "#0095F6" : $loginButton.style.backgroundColor = "#B9DFFC";
+}
+
+$inputContainer.addEventListener("keydown", handleLoginButton);
+
