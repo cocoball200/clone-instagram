8ac50190e43fcca1252d6b074df911fc993d331a
mia <cocoball200@gmail.com>
2021-01-19 14:17:15 +0900

    comment add done

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 const $commentList = document.querySelector(".comment-list");
-const $commentInput = document.querySelector(".comment-input").value; //textarea
+const $commentInput = document.querySelector(".comment-input"); //textarea
 const $commentForm = document.querySelector(".comment-write-form");
 const $commentSubmitButton = document.querySelector(".comment-submit-button");
+
 
 const handleWriting = (event) => {
     if (event.keyCode === 13) {
@@ -13,6 +14,17 @@ const handleWriting = (event) => {
 };
 
 const handleSubmit = (event) => {
-
+    const $eachCommentDiv = document.createElement("div");
+    $eachCommentDiv.setAttribute("class", "each-comment-list")
+    const $commentedId = document.createElement("p");
+    $commentedId.setAttribute("class", "commented-user-id")
+    const $commentedText = document.createElement("p");
+    $commentedText.setAttribute("class", "commented-text");
+    $commentedId.innerText = "hohohosia";
+    $commentedText.innerText = event.target.value;
+    $eachCommentDiv.appendChild($commentedId);
+    $eachCommentDiv.appendChild($commentedText);
+    $commentList.appendChild($eachCommentDiv);
+    event.target.value = "";
 }
 $commentInput.addEventListener("keydown", handleWriting);
