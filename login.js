2cc0228f3a43f815ead74f2527e4398021f30d86
mia <cocoball200@gmail.com>
2021-01-19 16:11:16 +0900

    login validate done

--- login.js
+++ login.js
@@ -3,8 +3,11 @@ const $inputID = document.querySelector(".input-id");
 const $inputPassword = document.querySelector(".input-password");
 const $loginButton = document.querySelector(".login-button");
 
+const validateLogin = () => {
+    $inputID.value.indexOf("@") !== -1 && $inputPassword.value.length >= 5 ? $loginButton.style.backgroundColor = "#0095F6" : $loginButton.style.backgroundColor = "#B9DFFC";
+}
 const handleLoginButton = () => {
-    $inputID.value.length >= 1 && $inputPassword.value.length >= 5 ? $loginButton.style.backgroundColor = "#0095F6" : $loginButton.style.backgroundColor = "#B9DFFC";
+    $inputID.value.length >= 1 && $inputPassword.value.length >= 5 && validateLogin();
 }
 
 $inputContainer.addEventListener("keydown", handleLoginButton);
--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const $commentList = document.querySelector(".comment-list");
-const $commentInput = document.querySelector(".comment-input"); //textarea
+//const $commentInput = document.getElementsByClassName("comment-input")[0];  // querySelector(".comment-input"); //textarea
+const $commentInput = document.querySelector(".comment-input")
 const $commentForm = document.querySelector(".comment-write-form");
 const $commentSubmitButton = document.querySelector(".comment-submit-button");
+const $commentedTextRemove = document.querySelector(".commented-text-remove");
+
+const $commentListAll = document.querySelector(".comment-list").children;
+console.log($commentListAll)
+
 
 
 const handleWriting = (event) => {
@@ -20,11 +26,29 @@ const handleSubmit = (event) => {
     $commentedId.setAttribute("class", "commented-user-id")
     const $commentedText = document.createElement("p");
     $commentedText.setAttribute("class", "commented-text");
-    $commentedId.innerText = "hohohosia";
-    $commentedText.innerText = event.target.value;
+    const $commentedTextRemove = document.createElement("P");
+    $commentedTextRemove.setAttribute("class", "commented-text-remove");
+    $commentedId.textContent = "Sia💖";
+    $commentedText.textContent = $commentInput.value;
+    $commentedTextRemove.textContent = "삭제";
     $eachCommentDiv.appendChild($commentedId);
     $eachCommentDiv.appendChild($commentedText);
+    $eachCommentDiv.appendChild($commentedTextRemove);
     $commentList.appendChild($eachCommentDiv);
-    event.target.value = "";
+    $commentInput.value = "";
+    $commentInput.removeEventListener("keydown", handleSubmit, true);
+    $commentSubmitButton.removeEventListener("click", handleSubmit, true);
 }
+
+const removeComment = (event) => {
+    console.log($commentListAll)
+
+}
+
+// const init = () => {
+
+// }
+
 $commentInput.addEventListener("keydown", handleWriting);
+$commentSubmitButton.addEventListener("click", handleSubmit);
+$commentedTextRemove.addEventListener("click", removeComment);
