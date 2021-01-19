715a08b95c3ddc6b2d1118d49e9ace097e59d696
mia <cocoball200@gmail.com>
2021-01-19 18:23:21 +0900

    modify searchbar

--- main.js
+++ main.js
@@ -1,13 +1,12 @@
 const $commentList = document.querySelector(".comment-list");
-//const $commentInput = document.getElementsByClassName("comment-input")[0];  // querySelector(".comment-input"); //textarea
 const $commentInput = document.querySelector(".comment-input")
 const $commentForm = document.querySelector(".comment-write-form");
 const $commentSubmitButton = document.querySelector(".comment-submit-button");
 const $commentedTextRemove = document.querySelector(".commented-text-remove");
-
 const $commentListAll = document.querySelector(".comment-list").children;
-console.log($commentListAll)
+const $inputSearchBar = document.querySelector(".input-search-bar");
 
+const IdList = ["ho_ho_ho", "like_birds_set_free", "chandelier", "snowman", "cheap_thrills", "courage_to_change", "titanium", "elastic_heart", "move_your_body", "hey_boy", "candy_cane_lane"];
 
 
 const handleWriting = (event) => {
@@ -36,19 +35,22 @@ const handleSubmit = (event) => {
     $eachCommentDiv.appendChild($commentedTextRemove);
     $commentList.appendChild($eachCommentDiv);
     $commentInput.value = "";
+    console.log($commentListAll)
     $commentInput.removeEventListener("keydown", handleSubmit, true);
     $commentSubmitButton.removeEventListener("click", handleSubmit, true);
 }
 
 const removeComment = (event) => {
     console.log($commentListAll)
-
 }
 
-// const init = () => {
+const handleSearch = (event) => {
+    const target = $inputSearchBar.value;
+    IdList.in
 
-// }
+}
 
 $commentInput.addEventListener("keydown", handleWriting);
 $commentSubmitButton.addEventListener("click", handleSubmit);
 $commentedTextRemove.addEventListener("click", removeComment);
+$inputSearchBar.addEventListener("keydown", handleSearch)
