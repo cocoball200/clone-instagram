416771493fd879eed2c2948ac43633263c7b2802
mia <cocoball200@gmail.com>
2021-01-20 20:52:10 +0900

    Add: search Function

--- js/login.js
+++ js/login.js
@@ -8,4 +8,3 @@ const validateLogin = () => {
 }
 
 $inputContainer.addEventListener("keydown", validateLogin);
-
--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
-
-
 const $commentList = document.querySelector(".comment-list");
 const $commentInput = document.querySelector(".comment-input")
 const $commentForm = document.querySelector(".comment-write-form");
 const $commentSubmitButton = document.querySelector(".comment-submit-button");
-const $commentedTextRemove = document.querySelector(".commented-text-remove");
-const $commentListAll = document.querySelector(".comment-list").children;
 const $inputSearchBar = document.querySelector(".input-search-bar");
+const $searchedIdDiv = document.querySelector(".searched-id-list");
+
 const IdList = ["ho_ho_ho", "like_birds_set_free", "chandelier", "snowman", "cheap_thrills", "courage_to_change", "titanium", "elastic_heart", "move_your_body", "hey_boy", "candy_cane_lane"];
 
 const handleWriting = (event) => {
     if (event.keyCode === 13) {
         if (!event.shiftKey) {
-            event.preventDefault(); // 서브밋이 안됨. 
+            event.preventDefault();
             handleSubmit(event);
         }
     }
@@ -20,27 +18,48 @@ const handleWriting = (event) => {
 
 const handleSubmit = (event) => {
     const $eachCommentDiv = document.createElement("div");
-    $eachCommentDiv.className = "each-comment-list";
+    $eachCommentDiv.className = `each-comment-list`;
     $eachCommentDiv.innerHTML = [
-        `<p class="commented-user-id">Sia❣️</p>`,
+        `<p class="commented-user-id">Sia</p>`,
         `<p class="commented-text"> ${$commentInput.value}</p>`,
-        `<button class="commented-text-remove">삭제</button>`
+        `<p class="commented-text-remove">삭제</p>`
     ].join("")
     $commentList.appendChild($eachCommentDiv);
     $commentInput.value = "";
+    const $commentedTextRemoveAll = document.querySelectorAll(".commented-text-remove");
+    $commentedTextRemoveAll.forEach(item => item.addEventListener("click", removeComment));
     $commentInput.removeEventListener("keydown", handleSubmit, true);
     $commentSubmitButton.removeEventListener("click", handleSubmit, true);
 }
 
 const removeComment = (event) => {
-    alert("hey");
+    const target = event.path[1];
+    target.remove();
 }
 
 const handleSearch = (event) => {
-    const target = $inputSearchBar.value;
+    const declaredSearchTags = document.querySelectorAll(".searched-results");
+    if (declaredSearchTags) {
+        declaredSearchTags.forEach(item => item.remove())
+    };
+    const $searchResults = document.createElement("div");
+    $searchResults.className = "searched-results"
+    $searchResults.textContent = "";
+    if (event.keyCode === 13) {
+        if (!event.shiftKey) {
+            event.preventDefault();
+            const target = $inputSearchBar.value;
+            const targetResult = IdList.find(item => item.indexOf(target) !== -1);
+            if (targetResult) {
+                $searchResults.textContent = `${targetResult}`
+            } else {
+                $searchResults.textContent = `찾는 결과가 없습니다.`
+            }
+            $searchedIdDiv.appendChild($searchResults);
+        }
+    }
 }
 
 $commentInput.addEventListener("keydown", handleWriting);
 $commentSubmitButton.addEventListener("click", handleSubmit);
-$commentedTextRemove.addEventListener("click", removeComment);
 $inputSearchBar.addEventListener("keydown", handleSearch);
