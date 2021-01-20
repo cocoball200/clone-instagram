ada6f6762014e84020949e1196658531fc7f3520
mia <cocoball200@gmail.com>
2021-01-20 11:09:32 +0900

    modify writing comment

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 const $commentList = document.querySelector(".comment-list");
 const $commentInput = document.querySelector(".comment-input")
 const $commentForm = document.querySelector(".comment-write-form");
@@ -5,9 +7,7 @@ const $commentSubmitButton = document.querySelector(".comment-submit-button");
 const $commentedTextRemove = document.querySelector(".commented-text-remove");
 const $commentListAll = document.querySelector(".comment-list").children;
 const $inputSearchBar = document.querySelector(".input-search-bar");
-
 const IdList = ["ho_ho_ho", "like_birds_set_free", "chandelier", "snowman", "cheap_thrills", "courage_to_change", "titanium", "elastic_heart", "move_your_body", "hey_boy", "candy_cane_lane"];
-
 
 const handleWriting = (event) => {
     if (event.keyCode === 13) {
@@ -20,37 +20,27 @@ const handleWriting = (event) => {
 
 const handleSubmit = (event) => {
     const $eachCommentDiv = document.createElement("div");
-    $eachCommentDiv.setAttribute("class", "each-comment-list")
-    const $commentedId = document.createElement("p");
-    $commentedId.setAttribute("class", "commented-user-id")
-    const $commentedText = document.createElement("p");
-    $commentedText.setAttribute("class", "commented-text");
-    const $commentedTextRemove = document.createElement("P");
-    $commentedTextRemove.setAttribute("class", "commented-text-remove");
-    $commentedId.textContent = "Sia💖";
-    $commentedText.textContent = $commentInput.value;
-    $commentedTextRemove.textContent = "삭제";
-    $eachCommentDiv.appendChild($commentedId);
-    $eachCommentDiv.appendChild($commentedText);
-    $eachCommentDiv.appendChild($commentedTextRemove);
+    $eachCommentDiv.className = "each-comment-list";
+    $eachCommentDiv.innerHTML = [
+        `<p class="commented-user-id">Sia❣️</p>`,
+        `<p class="commented-text"> ${$commentInput.value}</p>`,
+        `<button class="commented-text-remove">삭제</button>`
+    ].join("")
     $commentList.appendChild($eachCommentDiv);
     $commentInput.value = "";
-    console.log($commentListAll)
     $commentInput.removeEventListener("keydown", handleSubmit, true);
     $commentSubmitButton.removeEventListener("click", handleSubmit, true);
 }
 
 const removeComment = (event) => {
-    console.log($commentListAll)
+    alert("hey");
 }
 
 const handleSearch = (event) => {
     const target = $inputSearchBar.value;
-    IdList.in
-
 }
 
 $commentInput.addEventListener("keydown", handleWriting);
 $commentSubmitButton.addEventListener("click", handleSubmit);
 $commentedTextRemove.addEventListener("click", removeComment);
-$inputSearchBar.addEventListener("keydown", handleSearch)
+$inputSearchBar.addEventListener("keydown", handleSearch);
